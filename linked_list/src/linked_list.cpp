a9851ac288cb6d3344f9db2bbe41019cd941f417
Brijesh Patel <b.patel9528@gmail.com>
2019-09-07 12:17:50 -0600

    added initial project files

---
+++ linked_list/src/linked_list.cpp
@@ -0,0 +1,12 @@
+﻿// linked_list.cpp : Defines the entry point for the application.
+//
+
+#include "linked_list.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
