06e5a8eda9e1dd1dd9085c0c893fe72a64c4fdc0
Brijesh Patel <b.patel9528@gmail.com>
2019-09-29 14:53:38 -0600

    created quick_sort function with comparator

--- quick_sort/main.cpp
+++ quick_sort/main.cpp
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <iostream>
 #include "quick_sort.cpp"
+#include <string>
+
+struct Student
+{
+	int Id = 0;
+	std::string Name{ "" };
+};
+
+bool Compare(const Student& t_a, const Student& t_b);
 
 int main()
 {
@@ -15,4 +24,27 @@ int main()
 	{
 		std::cout << arr[i] << std::endl;
 	}
+
+	const int totalStudents = 5;
+
+	Student students[totalStudents];
+
+	for (int i = 0; i < totalStudents; i++)
+	{
+		students[totalStudents - 1 - i].Id = i;
+		students[i].Name = "A" + std::to_string(i);
+	}
+
+	my_lib::QuickSort(students, totalStudents, Compare);
+
+	for (int i = 0; i < totalStudents; i++)
+	{
+		std::cout << students[i].Id << " - " << students[i].Name << std::endl;
+	}
 }
+
+bool Compare(const Student& t_a, const Student& t_b)
+{
+	return (t_a.Id < t_b.Id);
+}
+
--- quick_sort/src/quick_sort.cpp
+++ quick_sort/src/quick_sort.cpp
@@ -12,18 +12,20 @@ namespace my_lib
 		void QuickSort(T t_arr[] , int t_left, int t_right)
 		{
 			if (t_left < 0 || t_left > t_right) return;
-			
-			int pivotIndex = t_right;
 
-			int l = 0;
+			T pivot = t_arr[t_right];
+
+			int l = t_left - 1;
 			for (int i = t_left; i < t_right; i++)
 			{
-				if (t_arr[i] < t_arr[pivotIndex])
+				if (t_arr[i] < pivot)
 				{
-					Swap(&t_arr[l++], &t_arr[i]);
+					Swap(&t_arr[++l], &t_arr[i]);
 				}
 			}
-			Swap(&t_arr[l++], &t_arr[pivotIndex]);
+			Swap(&t_arr[l + 1], &t_arr[t_right]);
+
+			int pivotIndex = l + 1;
 
 			QuickSort(t_arr, t_left, pivotIndex - 1);
 			QuickSort(t_arr, pivotIndex + 1, t_right);
@@ -40,7 +42,24 @@ namespace my_lib
 		template<class T>
 		void QuickSort(T t_arr[], int t_left, int t_right, bool t_compare(const T &t_a, const T &t_b))
 		{
+			if (t_left < 0 || t_left > t_right) return;
 
+			T pivot = t_arr[t_right];
+
+			int l = t_left - 1;
+			for (int i = t_left; i < t_right; i++)
+			{
+				if (t_compare(t_arr[i], pivot))
+				{
+					Swap(&t_arr[++l], &t_arr[i]);
+				}
+			}
+			Swap(&t_arr[l + 1], &t_arr[t_right]);
+
+			int pivotIndex = l + 1;
+
+			QuickSort(t_arr, t_left, pivotIndex - 1, t_compare);
+			QuickSort(t_arr, pivotIndex + 1, t_right, t_compare);
 		}
 	}
 
